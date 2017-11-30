3a4886a4b6407b5b7f49879cb90787ba4e4dabab
nasahu <nasahu@DIN66007718.corp.capgemini.com>
2017-11-30 14:41:06 +0530

    Spring with Jpa and Hibernate

--- springwithjpahibernate/src/main/java/com/capgemini/model/Activity.java
+++ springwithjpahibernate/src/main/java/com/capgemini/model/Activity.java
@@ -1,15 +1,32 @@
 package com.capgemini.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Activity {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
 	private String desc;
 
 	public String getDesc() {
 		return desc;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
 	public void setDesc(String desc) {
 		this.desc = desc;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 	
 }
--- springwithjpahibernate/src/main/java/com/capgemini/model/Exercise.java
+++ springwithjpahibernate/src/main/java/com/capgemini/model/Exercise.java
@@ -1,28 +1,43 @@
 package com.capgemini.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
 public class Exercise {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
 	
 	@Range(min = 1, max = 120)
 	private int minutes;
-	
+
 	@NotNull
 	private String activity;
 
 	public String getActivity() {
 		return activity;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
 
 	public void setActivity(String activity) {
 		this.activity = activity;
 	}
 
-	public int getMinutes() {
-		return minutes;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
